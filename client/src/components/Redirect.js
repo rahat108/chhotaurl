355d1705ba616b94b9d42115c615620f7a4f4291
rahat108 <61373565+rahat108@users.noreply.github.com>
2020-04-01 14:42:14 +0530

    Add files via upload

---
+++ client/src/components/Redirect.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import axios from "axios";
+
+class Redirect extends Component {
+
+    state = {
+        redirectURL: ''
+    };
+
+    componentDidMount() {
+        const hash = this.props.match.params.hash;
+
+        axios.get('/api/redirect',{
+            headers: {
+                hash
+            }
+        })
+        .then(res => {
+            console.log(res.data);
+            if (res.data.url) {
+                window.location.replace(res.data.url);
+            }
+
+        })
+        .catch(err => {
+            this.setState({
+                redirectURL: 'URL Not found'
+            })
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.redirectURL}</p>
+            </div>
+        );
+    }
+}
+
+export default Redirect;
